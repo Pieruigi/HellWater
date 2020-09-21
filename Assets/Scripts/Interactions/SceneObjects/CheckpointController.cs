f7a4ead9072e556fc2aebfbec7f475be37a7672f
Pierpiero <pierluigi.zomparelli@gmail.com>
2020-09-21 19:10:06 +0200

    wip gameplay

---
+++ Assets/Scripts/Interactions/SceneObjects/CheckpointController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HW.CachingSystem;
+
+namespace HW
+{
+    public class CheckpointController : MonoBehaviour
+    {
+       
+        [SerializeField]
+        int spawnPointId = -1;
+
+        FiniteStateMachine fsm;
+
+        private void Awake()
+        {
+            fsm = GetComponent<FiniteStateMachine>();
+            fsm.OnStateChange += HandleOnStateChange;
+        }
+
+       
+        void HandleOnStateChange(FiniteStateMachine fsm, int oldState)
+        {
+            if(oldState >= 0 && fsm.CurrentStateId < 0)
+            {
+                // Set the spawn point
+                Spawner.GetSpawner(PlayerController.Instance.transform).SpawnPointId = spawnPointId;
+
+                // Save
+                CacheManager.Instance.Save();
+            }
+        }
+    }
+
+}
