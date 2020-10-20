c9d78bdf07187296762f046e4b27613a5971f0f6
Pierpiero <pierluigi.zomparelli@gmail.com>
2020-10-20 12:16:08 +0200

    in game tutorial wip

---
+++ Assets/Scripts/Interactions/InGameTutorialController.cs
@@ -0,0 +1,56 @@
+﻿using HW.Interfaces;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace HW
+{
+    public class InGameTutorialController : MonoBehaviour, IInteractable
+    {
+        public UnityAction<int> OnShow;
+
+        [SerializeField]
+        int tutorialIndex;
+
+        FiniteStateMachine fsm;
+        int stateDisabled = -1;
+        
+        private void Awake()
+        {
+            fsm = GetComponent<FiniteStateMachine>();
+            fsm.OnStateChange += HandleOnStateChange;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Interact()
+        {
+            fsm.Lookup();
+        }
+
+        public bool IsAvailable()
+        {
+            return fsm.CurrentStateId != stateDisabled;
+        }
+
+
+        void HandleOnStateChange(FiniteStateMachine fsm, int oldState)
+        {
+            // Since the new state is -1 we are sure the come from the enable state
+            //InGameTutorialViewer.Instance.Show(tutorialIndex);
+            OnShow?.Invoke(tutorialIndex);
+        }
+    }
+
+}
---
+++ Assets/Scripts/UI/InGameTutorialViewer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HW.UI
+{
+    public class InGameTutorialViewer : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject panel;
+
+        public static InGameTutorialViewer Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (!Instance)
+            {
+                Instance = this;
+                Hide();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            InGameTutorialController[] list = FindObjectsOfType<InGameTutorialController>();
+            foreach (InGameTutorialController l in list)
+                l.OnShow += HandleOnShow;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+
+        void HandleOnShow(int tutorialId)
+        {
+            panel.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            panel.SetActive(false);
+        }
+    }
+
+}
