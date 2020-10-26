9417fb83ec3b0c823e25d059c333974a277c6d76
Pierpiero <pierluigi.zomparelli@gmail.com>
2020-10-26 18:45:35 +0100

    working on lighting

---
+++ Assets/Scripts/Misc/LightFlicker.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HW
+{
+	public class LightFlicker : MonoBehaviour
+	{
+
+		public bool flicker = true;
+
+		public float flickerIntensity = 0.5f;
+
+		private float baseIntensity;
+		public float BaseIntensity
+		{
+			get { return baseIntensity; }
+			set { baseIntensity = value; }
+		}
+
+		private Light lightComp;
+
+
+		void Awake()
+		{
+			lightComp = gameObject.GetComponent<Light>();
+			baseIntensity = lightComp.intensity;
+		}
+
+
+		void Update()
+		{
+			if (flicker)
+			{
+				float noise = Mathf.PerlinNoise(Random.Range(0.0f, 1000.0f), Time.time);
+				lightComp.intensity = Mathf.Lerp(baseIntensity - flickerIntensity, baseIntensity, noise);
+			}
+		}
+
+	}
+
+}
