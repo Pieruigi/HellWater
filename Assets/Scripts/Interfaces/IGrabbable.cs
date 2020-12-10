ddd0bf13c51d46d870a2a262bacbe9e1e06aaae6
Pierpiero <pierluigi.zomparelli@gmail.com>
2020-12-10 18:36:14 +0100

    player controller wip

---
+++ Assets/Scripts/Interfaces/IGrabbable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HW.Interfaces
+{
+    public interface IGrabbable
+    {
+
+        void GetGrabbed(GameObject grabber);
+    }
+
+}
